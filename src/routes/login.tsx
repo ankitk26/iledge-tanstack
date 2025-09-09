c3538bc72c360f4742baa867768b9655deff97bd
ankitk26 <myselfankit51@gmail.com>
2025-09-09 07:47:00 +0530

    show logging state

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { LoaderIcon } from "lucide-react";
+import { useState } from "react";
 import GithubIcon from "~/components/login/github-icon";
 import { Button } from "~/components/ui/button";
 import { authClient } from "~/lib/auth-client";
@@ -8,22 +10,32 @@ export const Route = createFileRoute("/login")({
 });
 
 function LoginPage() {
+  const [isLogging, setIsLogging] = useState(false);
+
   return (
     <div className="flex flex-col h-screen items-center justify-center py-12">
-      <div className="border p-10 flex flex-col items-center rounded-lg">
+      <div className="border space-y-8 p-10 flex flex-col items-center rounded-lg">
         <h2 className="text-2xl font-bold">Log in</h2>
         <Button
-          className="mt-8"
+          className="w-60"
           size="lg"
           onClick={async () => {
+            setIsLogging(true);
             await authClient.signIn.social({
               provider: "github",
               callbackURL: "/",
             });
+            setIsLogging(false);
           }}
         >
-          <GithubIcon />
-          Sign in with Github
+          {isLogging ? (
+            <LoaderIcon className="animate-spin" />
+          ) : (
+            <>
+              <GithubIcon />
+              Sign in with Github
+            </>
+          )}
         </Button>
       </div>
     </div>
